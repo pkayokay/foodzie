0f4b77f9f045ae97dcd92e9ecf070c26524e81b5
Paul Kim <pkayokay@gmail.com>
2016-10-14 15:44:35 +0000

    places in ascending order

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 5)
+    @places = Place.all.order("name ASC").paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
