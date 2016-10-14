6954a5f2b53f51d237c2b93c9fa0362b9fdc568b
Paul Kim <pkayokay@gmail.com>
2016-10-14 16:49:38 +0000

    destroy method and button

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,9 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+
+
+  validates :name,  presence: true, length: { maximum: 100, minimum: 3}
+  validates :description, presence: true, length: { maximum: 220, minimum: 7}
+  validates :address, presence: true, length: { maximum: 200, minimum: 10}
+
 end
