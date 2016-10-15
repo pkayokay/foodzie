434aa986e2027f0f6513bcb001347852e41f997b
Paul Kim <pkayokay@gmail.com>
2016-10-15 22:23:38 +0000

    comments done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+    RATINGS = {
+    'One star'    => '1',
+    'Two stars'   => '2',
+    'Three stars' => '3',
+    'Four stars'  => '4',
+    'Five stars'  => '5'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
+
 end
