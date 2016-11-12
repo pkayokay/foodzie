8fd2d894de909b2e62af865ae54d6fdbd7479bca
Paul Kim <pkayokay@gmail.com>
2016-11-12 20:22:29 +0000

    added username, first, last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :places
   has_many :comments
+  validates :email, presence: true
+  validates :firstname, presence: true, length: { maximum: 20 }
+  validates :lastname, presence: true, length: { maximum: 20 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112200019) do
+ActiveRecord::Schema.define(version: 20161112200845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 20161112200019) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
