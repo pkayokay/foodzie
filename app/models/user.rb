f615918d46664b1e021596e69660d62d2d4fde71
Paul Kim <pkayokay@gmail.com>
2016-11-12 21:00:28 +0000

    friendly_id for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
   has_many :places
   has_many :comments
-  validates :email, presence: true
-  validates :firstname, presence: true, length: { maximum: 20 }
-  validates :lastname, presence: true, length: { maximum: 20 }
-  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+  # validates :firstname, presence: true
+  # validates :lastname, presence: true
+  # validates :username, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  extend FriendlyId
+  friendly_id :username, use: [:slugged, :history]
 end
