010b5a0e6b3a5799b28618f4c1d38c9dc8887a48
Paul Kim <pkayokay@gmail.com>
2016-11-20 01:55:03 +0000

    sign up button responsiveness

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all.order("name ASC").paginate(:page => params[:page], :per_page => 3)
+    @places = Place.all.order("name ASC").paginate(:page => params[:page], :per_page => 4)
   end
 
   def new
