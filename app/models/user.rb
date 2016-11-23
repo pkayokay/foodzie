38b920c3a0dbffe504fe7cbd29a23f65fd21098c
Paul Kim <pkayokay@gmail.com>
2016-11-23 20:24:11 +0000

    fixed firstname lastname sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   has_many :places
   has_many :comments
-  # validates :firstname, presence: true
-  # validates :lastname, presence: true
-  # validates :username, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
